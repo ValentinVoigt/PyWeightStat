7868eab988058e8a2af58c069c3b69d56e9095ea
Valentin Voigt <mail@valentinvoigt.info>
2014-02-15 15:56:28 +0100

    Renamed home view

--- pyweightstat/views.py
+++ pyweightstat/views.py
@@ -3,7 +3,7 @@ from pyramid.view import view_config
 from .models import Weight
 
 @view_config(route_name='home', renderer='templates/home.pt')
-def my_view(request):
+def home(request):
     latest = Weight.get_latest(10)
     _min = min([int(i.weight) for i in latest])
     _max = max([int(i.weight) for i in latest])
